8cf052e52b23d0a6af9c192fc82cc1e7eaca42ce
coder2711 <73233141+coder2711@users.noreply.github.com>
2021-03-27 10:53:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+canvas=document.getElementById("canva");
+ctx=canvas.getContext("2d");
+
+color= "blue";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=7;
+ctx.arc(250,200,40,0,2*Math.PI);
+ctx.stroke();
+
+color= "black";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=7;
+ctx.arc(340,200,40,0,2*Math.PI);
+ctx.stroke();
+
+color= "red";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=7;
+ctx.arc(430,200,40,0,2*Math.PI);
+ctx.stroke();
+
+color= "yellow";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=7;
+ctx.arc(300,250,40,0,2*Math.PI);
+ctx.stroke();
+
+color= "green";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=7;
+ctx.arc(390,250,40,0,2*Math.PI);
+ctx.stroke();
